f9240a2aad8f24ddd630e2486a029fe570741aee
Rohit <mallavarapurohit@gmail.com>
2025-11-27 00:12:38 +0530

    Clean up duplicate code in PostHogPageView component

--- components/PostHogPageView.tsx
+++ components/PostHogPageView.tsx
@@ -24,40 +24,6 @@ function PostHogPageViewContent() {
   return null;
 }
 
-// Wrap in Suspense to avoid client-side de-opt errors
-export default function PostHogPageView() {
-  return (
-    <Suspense fallback={null}>
-      <PostHogPageViewContent />
-    </Suspense>
-  );
-}'use client';
-
-import { usePathname, useSearchParams } from "next/navigation";
-import { useEffect, Suspense } from "react";
-import { usePostHog } from "posthog-js/react";
-
-function PostHogPageViewContent() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const posthog = usePostHog();
-
-  useEffect(() => {
-    if (pathname && posthog) {
-      let url = window.origin + pathname;
-      if (searchParams.toString()) {
-        url = url + `?${searchParams.toString()}`;
-      }
-      posthog.capture('$pageview', {
-        '$current_url': url,
-      });
-    }
-  }, [pathname, searchParams, posthog]);
-
-  return null;
-}
-
-// Wrap in Suspense to avoid client-side de-opt errors
 export default function PostHogPageView() {
   return (
     <Suspense fallback={null}>
